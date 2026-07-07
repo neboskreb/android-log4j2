dd99890f4e2c3456a0e3a318d8707ab4342ad750
John Y. Pazekha <neboskreb@narod.ru>
2026-07-07 23:24:58 +0200

    Removed the workaround for the missing JMX infrastructure

--- library/src/main/java/java/lang/management/RuntimeMXBean.java
+++
@@ -1,9 +0,0 @@
-package java.lang.management;
-
-import java.util.List;
-
-// FIXME Remove this once 2.24.0 is released
-// https://github.com/apache/logging-log4j2/issues/2774#issuecomment-2254418643
-public interface RuntimeMXBean {
-    public List<String> getInputArguments();
-}
